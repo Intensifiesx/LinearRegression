868cf246a8f7c9cd7f88cac0809c1192901e168d
Alex Akoopie <alexakoopie@gmail.com>
2023-06-04 20:12:43 -0700

    Fixed tensor sizes
    Reshaped the tensor to match the size for training.

--- main.py
+++ main.py
@@ -20,24 +20,16 @@ print(f'Using {device} device')
 
 # %%
 #======PART 1======
-x = np.random.uniform(-1, 1, size = 100)
+x = np.random.uniform(-1, 1, size = (100, 1))
 y = 4.3 * x + 5
 
 # %%
 #======PART 2======
-idx = np.arange(100)
-np.random.shuffle(idx)
-train_idx = idx[:80]
-val_idx = idx[80:]
+x_train = x[:int(len(x)/(5/4))]
+x_val = x[int(len(x)/(5/4)):]
 
-x_train, y_train = x[train_idx], y[train_idx]
-x_val, y_val = x[val_idx], y[val_idx]
-
-# x_train = x[:int(len(x)/(5/4))]
-# x_val = x[int(len(x)/(5/4)):]
-
-# y_train = y[:int(len(y)/(5/4))]
-# y_val = y[int(len(y)/(5/4)):]
+y_train = y[:int(len(y)/(5/4))]
+y_val = y[int(len(y)/(5/4)):]
 
 # %%
 #======PART 3======
@@ -48,12 +40,12 @@ n_epochs = 1000 #Number of iterations
 # pt.nn.Linear(a, b) - PyTorch linear neural network
 # a - number of inputs (features)
 # b - number of outputs (labels)
-model = pt.nn.Sequential(pt.nn.Linear(1, 1)).float().to(device) #Creates a model and sends it to the device
+model = pt.nn.Sequential(pt.nn.Linear(1, 1)).to(device) #Creates a model and sends it to the device
 loss_fn = pt.nn.MSELoss() #Defines a MSE loss function
 optimizer = pt.optim.SGD(model.parameters(), lr) #Defines a stochastic gradient descent (SGD) optimizer to update the parameters
 
 #Training
-model.train(True) #Sets model to training mode
+model.train() #Sets model to training mode
 
 #Send the training data to PyTorch
 x_train = pt.from_numpy(x_train).float().to(device)
